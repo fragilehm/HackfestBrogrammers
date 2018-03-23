4c5adb28d9503b58b680aa7476bf0a5f7c7cec03
Khasanza Mukhamed <fragilehm2411@gmail.com>
2018-03-24 03:22:58 +0600

    base project added

---
+++ HackfestBrogrammers/HackfestBrogrammers/StartingPage/Controllers/StartingPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StartingPageViewController.swift
+//  HackfestBrogrammers
+//
+//  Created by Khasanza on 3/24/18.
+//  Copyright © 2018 Khasanza. All rights reserved.
+//
+
+import UIKit
+
+class StartingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
